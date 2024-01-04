9fac3dfc1133b56a5340e2fa35ebe80f12625546
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2024-01-04 18:23:47 +0400

    make it better

--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoURI='mongodb+srv://safi:35064612@cluster0-ags3s.mongodb.net/TomaBoutique?retryWrites=true&w=majority';
 
 mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology: true})
-const conn = mongoose.connection;
 
+const conn = mongoose.connection;
 conn.on('connected',()=>{
     console.log('you are connecting to database') 
 })
