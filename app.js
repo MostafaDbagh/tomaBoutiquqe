91e756b515dcfc0f73ed3e95f79c4fc2896cbee8
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2024-01-04 19:09:45 +0400

    make it better

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.use("/",contactSupportApi)
 
 app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
 
+if(process.env.NODE_ENV ==='production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 app.use((req,res,next)=>{
   const errorObj = {
     message:'not Found',
--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const http = require('http');
 const app= require('./app')
-const express = require('express');
 
 const port = process.env.PORT || 4060;
 const server = http.createServer(app);
 
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-      });
-}
+
 
 server.listen(port ,()=>console.log('we arr listening to the port '+port))
 
